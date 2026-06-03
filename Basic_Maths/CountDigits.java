3139ec1e54fadbc1cd1b2f465e6b2843378ed71f
NAMRUTHA SRINIDHI T <anamruthasrinidhi@gmail.com>
2026-06-03 23:43:02 +0530

    Implement logarithm method for digit counting

--- Basic_Maths/CountDigits.java
+++ Basic_Maths/CountDigits.java
@@ -1,3 +1,4 @@
+//Approach 1: Iterative / Division Method
 public class CountDigits {
 
     public static int countDigits(int n) {
@@ -13,6 +14,25 @@ public class CountDigits {
         }
 
         return count;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 12345;
+
+        System.out.println("Number of digits: " + countDigits(n));
+    }
+}
+
+//Approach 2: Logarithm Method
+public class CountDigits {
+
+    public static int countDigits(int n) {
+
+        if (n == 0)
+            return 1;
+
+        return (int)(Math.log10(n) + 1);
     }
 
     public static void main(String[] args) {
